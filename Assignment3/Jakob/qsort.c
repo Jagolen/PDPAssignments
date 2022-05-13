48619b6ce75ce8657fb96fa70a6f96130dc1acad
Jagolen <jakob.golen@gmail.com>
2022-05-13 15:06:03 +0200

    worked

--- Assignment3/Jakob/qsort.c
+++ Assignment3/Jakob/qsort.c
@@ -1,24 +1,105 @@
 #include "qsort.h"
 int main(int argc, char **argv){
     
-    if (3 != argc) {
-        printf("Usage: input_file output_file\n");
+    if (4 != argc) {
+        printf("Usage: input_file output_file method\n");
         return 1;
 	}
+
+	//Parameters
 	char *input_name = argv[1];
 	char *output_name = argv[2];
     double *timing;
-    int rank, size, list_size;
-    int *values;
+    int rank, size, list_size, expanded_size;
+    int *values, *local_values, *merge_array, *recieve_array;
+	int method = atoi(argv[3]);
 
+	//Initializing MPI
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	//Determining dimensions and parameters for cart_create
+	int dims = 0;
+	int elements = 1;
+	while(elements != size){
+		elements *= 2;
+		dims++;
+	}
+	int periodic[dims];
+	int proc_per_dim[dims];
+	for(int i = 0; i<dims; i++){
+		periodic[i] = 0;
+		proc_per_dim[i] = 2;
+	}
+
+	//Creating a hypercube communicator
+	MPI_Comm hyper;
+	MPI_Cart_create(MPI_COMM_WORLD, dims, proc_per_dim, periodic, 1, &hyper);
+	MPI_Comm_rank(hyper, &rank);
+
+//Split test
+/* 	int color = pow(2,2)*rank/size;
+	MPI_Comm new_comm;
+	MPI_Comm_split(hyper, color, rank, &new_comm);
+	int rank2,sz;
+	MPI_Comm_rank(new_comm,&rank2);
+	MPI_Comm_size(new_comm,&sz);
+	printf("Rank = %d, New rank = %d\n",rank, rank2);
+	MPI_Status status; */
+
+	//Rank 0 reads the input list and broadcast number of elements
     if(rank == 0){
-        if (0 > (list_size = read_input(input_name, &values))) return 2;
+        if (0 > (list_size = read_input(input_name, &values, size, &expanded_size))) return 2;
 		timing = (double*)malloc(size*sizeof(double));
+		printf("Size = %d, Expanded = %d\n",list_size, expanded_size);
     }
-    printf("list size = %d\nfirst 10 elements in list: ",list_size);
-    for(int i=0;i<10;i++){
-        printf("%d ",values[i]);
-    }   printf("\n");
+	MPI_Bcast(&list_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&expanded_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	//Creating local lists
+	local_values = (int*)malloc(expanded_size*sizeof(int));
+
+	//To merge the lists
+	merge_array = (int*)malloc(expanded_size*sizeof(int));
+
+	//To recieve elements from other processes
+	recieve_array = (int*)malloc(expanded_size*sizeof(int));
+
+	//number of elements in the local list
+	int nr_local_values = expanded_size/size;
+
+	//The values are scattered to the local lists and a timer starts
+	MPI_Scatter(values, nr_local_values, MPI_INT, local_values, nr_local_values, MPI_INT, 0, hyper);
+	MPI_Wtime();
+
+	//The first step is to sort each local array with the built in qsort function
+	qsort(local_values, nr_local_values, sizeof(int), cmpfunc);
+
+	//We do log_2(p) iterations = log_10(p)/log_10(2)
+	int number_of_iterations = round(log(size)/log(2));
+
+	//Now we start iterating
+	for(int iteration = 0; iteration<number_of_iterations; iteration++){
+		/*Based on the iteration, we split the communicator into groups,
+		a single group for iteration 0, 2 groups for iteration 1, 4 groups for iteration 2 and so on
+		*/
+		MPI_Comm hyper_sub;
+		int sub_rank, sub_size;
+		int group = pow(2,iteration)*rank/size;
+		MPI_Comm_split(hyper, group, rank, &hyper_sub);
+
+		//Get the size of the sub group and the new ranks
+		MPI_Comm_rank(hyper_sub, &sub_rank);
+		MPI_Comm_size(hyper_sub, &sub_size);
+
+		//We get the median for every process
+		int median = local_values[nr_local_values/2];
+
+
+
+		//Free the split communicator so a new one can be used in the next iteration
+		MPI_Comm_free(&hyper_sub);
+	}
 
 
     /* writing the output */
@@ -29,25 +110,40 @@ int main(int argc, char **argv){
 		}
     }
 
+
+	MPI_Finalize();
     return 0;
 }
 
-int read_input(const char *file_name, int **values) {
+
+//Function for reading the input
+int read_input(const char *file_name, int **values, int size, int *expanded_size) {
 	FILE *file;
 	if (NULL == (file = fopen(file_name, "r"))) {
 		perror("Couldn't open input file");
 		return -1;
 	}
 	int num_values;
+	int expanded_values;
 	if (EOF == fscanf(file, "%d", &num_values)) {
 		perror("Couldn't read element count from input file");
 		return -1;
 	}
-	if (NULL == (*values = malloc(num_values * sizeof(double)))) {
+
+	int remainder = size - (num_values%size);
+	if(num_values%size == 0) remainder = 0;
+	expanded_values = num_values + remainder;
+	printf("Expanded = %d\n", expanded_values);
+
+	if (NULL == (*values = malloc(expanded_values * sizeof(int)))) {
 		perror("Couldn't allocate memory for input");
 		return -1;
 	}
-	for (int i=0; i<num_values; i++) {
+	for(int i = 0; i< remainder; i++){
+		(*values)[i] = 0;
+	}
+
+	for (int i=remainder; i<expanded_values; i++) {
 		if (EOF == fscanf(file, "%d", &((*values)[i]))) {
 			perror("Couldn't read elements from input file");
 			return -1;
@@ -56,9 +152,12 @@ int read_input(const char *file_name, int **values) {
 	if (0 != fclose(file)){
 		perror("Warning: couldn't close input file");
 	}
+	*expanded_size = expanded_values;
 	return num_values;
 }
 
+
+//Function for writing the output
 int write_output(const char *file_name, int *sorted_list, int list_size) {
 	FILE *file;
 	if (NULL == (file = fopen(file_name, "w"))) {
@@ -77,4 +176,9 @@ int write_output(const char *file_name, int *sorted_list, int list_size) {
 		perror("Warning: couldn't close output file");
 	}
 	return 0;
+}
+
+//The standard comparison function for qsort
+int cmpfunc(const void *a, const void *b){
+	return (*(int*)a - *(int*)b);
 }
--- Assignment3/Jakob/qsort.h
+++ Assignment3/Jakob/qsort.h
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-int read_input(const char *file_name, int **values);
+int read_input(const char *file_name, int **values, int size, int *expanded_size);
 
 int write_output(const char *file_name, int *sorted_list, int list_size);
+
+int cmpfunc(const void *a, const void *b);
